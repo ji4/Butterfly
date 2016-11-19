78df3ae96e8a52872044de98dd39790b0f42ad08
howdyhi <money@AnonymousdeMacBook-Air.local>
2016-11-19 18:31:07 +0800

    button

--- app/src/main/java/yzu/money/butterfly/MainActivity.java
+++ app/src/main/java/yzu/money/butterfly/MainActivity.java
@@ -2,12 +2,21 @@ package yzu.money.butterfly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
